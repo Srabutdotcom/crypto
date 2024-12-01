4ca38e7a0c4ca453e34b288a8170ac02d9a26e9d
handoko <srabutdotcom@gmail.com>
2024-12-01 13:09:56 +0700

    rev

--- test/hkdfexpand_bench.js
+++ test/hkdfexpand_bench.js
@@ -1,7 +1,7 @@
 import * as hkdf from "@noble/hashes/hkdf"
 import { sha256, sha384, sha512 } from "@noble/hashes/sha2"
 import { EarlySecret } from "../src/hkdf/hkdf.js"
-import { hkdfExpand, hkdfExpand2 } from "../src/hkdf/hkdf.js"
+import { hkdfExpand } from "../src/hkdf/hkdf.js"
 
 const rnd = crypto.getRandomValues(new Uint8Array(32))
 
